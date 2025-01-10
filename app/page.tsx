529a5e90414a2a1d3da3a8b0d590560c9642fbc1
AtanasovCode <a_ljupce@yahoo.com>
2025-01-10 20:45:27 +0100

    minor responsiveness improvements

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,13 @@ import CycleventureLogo from "@/app/ui/cycleventure-logo";
 
 export default function Home() {
   return (
-    <div className="min-h-dvh flex flex-col items-center justify-start gap-6 bg-background text-text">
-      <div className="w-full flex items-center justify-center py-8 z-50 md:py-10 lg:py-14">
+    <div className="min-h-dvh flex flex-col items-center justify-start gap-24 py-8 bg-background text-text">
+      <div className="w-full flex items-center justify-center z-50">
         <CycleventureLogo />
       </div>
       <div className="w-full flex flex-col items-center justify-center z-50 gap-16 lg:gap-20 xl:gap-24 px-8 md:px-12 lg:px-20 xl:px-28 lg:flex-row">
         <div className="flex flex-col items-center justify-center gap-4 md:w-2/3 lg:w-1/2 xl:w-1/3">
-          <div className={`flex flex-col items-start justify-center font-bold text-4xl md:text-5xl lg:text-6xl`}>
+          <div className={`flex flex-col items-start justify-center font-bold text-5xl lg:text-6xl`}>
             Explore, Experience, Enjoy Life!
           </div>
           <div className="text-left text-base">
@@ -24,7 +24,7 @@ export default function Home() {
             />
           </div>
         </div>
-        <div className="flex items-center justify-center rounded-xl overflow-hidden lg:w-1/2">
+        <div className="flex items-center justify-center rounded-xl overflow-hidden md:w-2/3  lg:w-1/2">
           <Image
             src="/hero-mobile.jpg"
             width={640}
