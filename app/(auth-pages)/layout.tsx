9784168fcc58fd2f6d217b36c381201532e022a6
AtanasovCode <a_ljupce@yahoo.com>
2025-01-30 18:12:48 +0100

    added different images for different auth paths

--- app/(auth-pages)/layout.tsx
+++ app/(auth-pages)/layout.tsx
@@ -1,32 +1,74 @@
+'use client';
+
+
+import { useEffect } from "react";
+import { usePathname } from "next/navigation";
 import Image from "next/image";
 
-export default async function Layout({
+type ImageSet = {
+  mobile: string;
+  tablet: string;
+  desktop: string;
+  alt: string;
+}
+
+type Images = Record<string, ImageSet>;
+
+export default function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+
+  const pathname = usePathname();
+
+  // Define image mapping based on routes
+  const images: Images = {
+    "/sign-in": {
+      mobile: "/auth-mobile.jpg",
+      tablet: "/auth-tablet.jpg",
+      desktop: "/auth-desktop.jpg",
+      alt: "Photo of a mountain bike"
+    },
+    "/sign-up": {
+      mobile: "/signup-mobile.jpg",
+      tablet: "/signup-tablet.jpg",
+      desktop: "/signup-desktop.jpg",
+      alt: "photo of a mountain bike"
+    },
+    "/forgot-password": {
+      mobile: "/forgot-mobile.jpg",
+      tablet: "/forgot-tablet.jpg",
+      desktop: "/forgot-desktop.jpg",
+      alt: "Photo of a group of 3 riders on mountain bikes climibing up a hill"
+    },
+  };
+
+  // Default to sign-in images if route is not mapped
+  const selectedImages = images[pathname] || images["/sign-in"];
+
   return (
     <div className="h-dvh flex items-start justify-center bg-background">
       <div className="flex items-center justify-center max-h-dvh overflow-hidden">
         <Image
-          src="/auth-mobile.jpg"
+          src={selectedImages.mobile}
           width={640}
           height={800}
-          alt="authentication image of a man and a mountain bike on a mountain"
+          alt={selectedImages.alt}
           className="block md:hidden"
         />
         <Image
-          src="/auth-tablet.jpg"
+          src={selectedImages.tablet}
           width={1920}
           height={1295}
-          alt="authentication image of a man and a mountain bike on a mountain"
+          alt={selectedImages.alt}
           className="hidden md:block xl:hidden"
         />
         <Image
-          src="/auth-desktop.jpg"
+          src={selectedImages.desktop}
           width={2400}
           height={1619}
-          alt="authentication image of a man and a mountain bike on a mountain"
+          alt={selectedImages.alt}
           className="hidden xl:block"
         />
       </div>
