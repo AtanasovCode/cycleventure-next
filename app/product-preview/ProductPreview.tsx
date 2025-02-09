25f0bbfaf6f04ecda040de6ab02f72986237c257
AtanasovCode <a_ljupce@yahoo.com>
2025-02-09 15:46:29 +0100

    added navigation to /product-preview route

--- app/product-preview/ProductPreview.tsx
+++ app/product-preview/ProductPreview.tsx
@@ -2,7 +2,9 @@
 
 import { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
+import { User } from "@supabase/supabase-js";
 import { fetchSelectedProduct } from "@/app/lib/data";
+import Navigation from "@/app/ui/Navigation";
 import Product from "@/app/ui/product-preview/Product";
 import { ProductType } from "@/app/types/product-preview";
 
@@ -13,6 +15,8 @@ export default function ProductPreview() {
     const [id, setID] = useState<string>("")
     const [product, setProduct] = useState<ProductType>();
     const [loading, setLoading] = useState<boolean>(false);
+    const [user, setUser] = useState<User | null>(null);
+
 
     useEffect(() => {
         const name = searchParams.get("name")?.split(",") || [];
@@ -35,20 +39,23 @@ export default function ProductPreview() {
     }, [id])
 
     return (
-        <div className="min-h-dvh flex items-center justify-center bg-background">
-            {
-                loading ? (
-                    <div>Loading...</div>
-                ) : (
-                    product ? (
-                        <Product product={product} />
+        <div className="min-h-dvh flex flex-col items-center justify-start bg-background gap-16">
+            <Navigation user={user} setUser={setUser} />
+            <div>
+                {
+                    loading ? (
+                        <div>Loading...</div>
                     ) : (
-                        <div>
-                            Product not found
-                        </div>
+                        product ? (
+                            <Product product={product} />
+                        ) : (
+                            <div>
+                                Product not found
+                            </div>
+                        )
                     )
-                )
-            }
+                }
+            </div>
         </div>
     );
 }
