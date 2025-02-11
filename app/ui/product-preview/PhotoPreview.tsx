ec155b34a0b8a0389e231de9bc398fd103ca4b9c
AtanasovCode <a_ljupce@yahoo.com>
2025-02-11 23:43:16 +0100

    improved accessibility for photo navigation buttons - added navigation using arrow keys - assigned button role to div wrappers for arrow navigation - assigedn 	abIndex to arrow navigation - added tab navigation functionality

--- app/ui/product-preview/PhotoPreview.tsx
+++ app/ui/product-preview/PhotoPreview.tsx
@@ -28,25 +28,26 @@ export default function PhotoPreview({
         })
     }, []);
 
-    useEffect(() => {}, [])
+    useEffect(() => { }, [])
 
     const prevPhoto = () => {
-        if (currentPhotoIdx === 0) {
-            setCurrentPhotoIdx(photos.length - 1)
-            return;
-        }
-
-        setCurrentPhotoIdx(currentPhotoIdx - 1);
+        setCurrentPhotoIdx((prevIdx) => prevIdx === 0 ? photos.length - 1 : prevIdx - 1)
     }
 
     const nextPhoto = () => {
-        if (currentPhotoIdx === photos.length - 1) {
-            setCurrentPhotoIdx(0)
-            return;
-        }
-
-        setCurrentPhotoIdx(currentPhotoIdx + 1);
+        setCurrentPhotoIdx((prevIdx) => prevIdx === photos.length - 1 ? 0 : prevIdx + 1)
     }
+
+    // Listen for global keydown events
+    useEffect(() => {
+        const handleKeyDown = (e: KeyboardEvent) => {
+            if (e.key === "ArrowLeft") prevPhoto();
+            if (e.key === "ArrowRight") nextPhoto();
+        };
+
+        document.addEventListener("keydown", handleKeyDown);
+        return () => document.removeEventListener("keydown", handleKeyDown);
+    }, []);
 
 
     return (
@@ -54,6 +55,9 @@ export default function PhotoPreview({
             <div
                 className="absolute top-1/2 -left-4 md:left-6 -translate-y-1/2 bg-primary flex items-center justify-center p-1 rounded-full cursor-pointer border-[.4rem] border-background"
                 onClick={() => prevPhoto()}
+                onKeyDown={(e) => e.key === "Enter" || e.key === "" ? prevPhoto() : null}
+                role="button"
+                tabIndex={0}
             >
                 <ArrowLeft className="w-5 md:w-8 h-auto" />
             </div>
@@ -67,6 +71,9 @@ export default function PhotoPreview({
             <div
                 className="absolute top-1/2 -right-4 md:right-6 -translate-y-1/2 bg-primary flex items-center justify-center p-1 rounded-full cursor-pointer border-[.4rem] border-background"
                 onClick={() => nextPhoto()}
+                onKeyDown={(e) => e.key === "Enter" || e.key === "" ? nextPhoto() : null}
+                role="button"
+                tabIndex={0}
             >
                 <ArrowRight className="w-5 md:w-8 h-auto" />
             </div>
