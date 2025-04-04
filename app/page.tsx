368a11e86cc58698e6d01ee6511ed413ca2a46f4
AtanasovCode <a_ljupce@yahoo.com>
2025-04-04 22:32:22 +0200

    add theme toggle to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,22 @@
+'use client'
+
 import Image from "next/image";
 import Link from "next/link";
 import CycleventureLogo from "@/app/ui/cycleventure-logo";
+import ThemeToggle from "@/app/ui/navigation/ThemeToggle";
 
 export default function Page() {
   return (
     <div className="min-h-dvh flex flex-col items-center justify-start gap-24 py-8 bg-background text-text">
-      <div className="w-full flex items-center justify-center z-50">
-        <CycleventureLogo />
+      <div className="w-full grid grid-cols-3 items-center justify-center z-50 px-4">
+        <div className="col-start-2 justify-self-center">
+          <CycleventureLogo />
+        </div>
+        <div className="col-start-3 justify-self-end cursor-pointer">
+          <ThemeToggle />
+        </div>
       </div>
+
       <div className="w-full flex flex-col items-center justify-center z-50 gap-16 lg:gap-20 xl:gap-24 px-8 md:px-12 lg:px-20 xl:px-28 lg:flex-row">
         <div className="flex flex-col items-center justify-center gap-4 md:w-2/3 lg:w-1/2 xl:w-1/3">
           <div className={`flex flex-col items-start justify-center font-bold text-5xl lg:text-6xl`}>
